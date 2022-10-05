f8e387bd36532667bed260c4f7d80010883827b1
Youngjun Choi <juni2004c@gmail.com>
2022-10-06 00:28:34 +0900

    feat:토큰 유무에 따른 페이지 이동 로직추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Login from './routes/login';
+import { useEffect, useState } from 'react';
+import Todo from './routes/todo';
 
 function App() {
+  const navigate = useNavigate();
+  const [token, setToken] = useState('');
+  useEffect(() => {
+    const prevToken = localStorage.getItem('loginToken');
+    prevToken && setToken(prevToken);
+    if (token) {
+      navigate('/todo');
+    } else {
+      navigate('/');
+    }
+  }, [token]);
   return (
     <Routes>
       <Route path='/' element={<Login></Login>}></Route>
-      <Route path='/todo'></Route>
+      <Route path='/todo' element={<Todo></Todo>}></Route>
     </Routes>
   );
 }
